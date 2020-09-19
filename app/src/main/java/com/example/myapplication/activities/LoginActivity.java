d13857036c646d52bd370884d7ac8fc9271dbf49
julio <juliointriago63@gmail.com>
2020-09-19 17:56:12 -0500

    Diseño de login y registro

--- app/src/main/java/com/example/myapplication/activities/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/activities/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.example.myapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+     Button btnregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnregister = findViewById(R.id.btnRegister);
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,register.class));
+            }
+        });
+
     }
+
+
 }
